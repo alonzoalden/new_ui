215e57e870650adfb3c382054cf94a86f3a4edf6
Alonzo <alonzoalden@gmail.com>
2019-06-05 14:55:55 -0700

    smartmenus fix

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -3,12 +3,32 @@ $(document).ready(function() {
   $('#main-menu').smartmenus({
       subMenusSubOffsetX: 1,
       subMenusSubOffsetY: -8,
-      noMouseOver: true
+      noMouseOver: true,
+      keepInViewport:		false,
+      showFunction:		function($ul, complete) {
+        $ul.slideDown(70, complete);
+      },
+      hideFunction:		function($ul, complete) {
+        $ul.slideUp(80, function() {
+          $ul.parent().removeClass('open');
+          complete();
+        });
+      }
   });
   $('#sub-menu').smartmenus({
       subMenusSubOffsetX: 1,
       subMenusSubOffsetY: -8,
-      noMouseOver: true
+      noMouseOver: true,
+      keepInViewport:		false,
+      showFunction:		function($ul, complete) {
+        $ul.slideDown(70, complete);
+      },
+      hideFunction:		function($ul, complete) {
+        $ul.slideUp(80, function() {
+          $ul.parent().removeClass('open');
+          complete();
+        });
+      }
   });
   
 });
